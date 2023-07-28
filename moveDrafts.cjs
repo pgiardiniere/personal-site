27d63aede39b5c43edb109470d2b936993956351
Peter Giardiniere <pgiardiniere@gmail.com>
2023-07-28 17:37:48 -0400

    Fix drafts movement feature

---
+++ moveDrafts.cjs
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+
+// Get the command-line arguments
+const args = process.argv.slice(2);
+
+// Define the directories based on the "reverse" argument
+let sourceDir = './drafts';
+let destinationDir = './src/lib/posts';
+if (args.includes('--reverse')) {
+  [sourceDir, destinationDir] = [destinationDir, sourceDir];
+}
+
+// Use glob to find all files matching filePattern
+const filePattern = 'draft-*';
+glob(filePattern, { cwd: sourceDir }, (err, files) => {
+  if (err) {
+    console.error('Error reading files:', err);
+    return;
+  }
+
+  // Move all matched files to the destination directory
+  files.forEach((file) => {
+    const sourcePath = path.join(sourceDir, file);
+    const destinationPath = path.join(destinationDir, path.basename(file));
+
+    fs.rename(sourcePath, destinationPath, (err) => {
+      if (err) {
+        console.error(`Error moving ${file}:`, err);
+      } else {
+        console.log(`${file} moved successfully.`);
+      }
+    });
+  });
+});
