bfa1f29ba1c68b6a71aa2dd298693aa3d20faa0e
Peter Giardiniere <pgiardiniere@gmail.com>
2023-08-02 17:05:13 -0400

    Add markdown support

---
+++ src/routes/contact/+page.md
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<h1>Contact</h1>
+
+Find me on [LinkedIn](https://www.linkedin.com/in/peter-giardiniere-136aaab9/) 
+& [GitHub](https://github.com/pgiardiniere).
--- src/routes/contact/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-	<title>Contact</title>
-</svelte:head>
-
-<h1>Contact</h1>
-
-<p>Find me on [LinkedIn](https://www.linkedin.com/in/peter-giardiniere-136aaab9/) 
-& [GitHub](https://github.com/pgiardiniere).</p>
