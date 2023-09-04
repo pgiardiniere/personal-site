02e2eefd0fc96066a8f64270c09b8bb09e3c994c
Peter Giardiniere <pgiardiniere@gmail.com>
2023-09-04 15:00:08 -0400

    Document config change

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -6,7 +6,14 @@ import { error } from '@sveltejs/kit'
 //
 // see:
 //    https://learn.svelte.dev/tutorial/csr
-//    https://kit.svelte.dev/docs/page-options 
+//    https://kit.svelte.dev/docs/page-options
+//
+// I don't love the way page transitions work, so to remove them just disable
+// client-side js entirely. The irony of using a JS framework to ship a no-JS
+// website is not lost on me :)
+// 
+// Link prefetching would be nice, but pages load incredibly fast anyway since
+// they're all just static content.
 export const csr = false
 
 export const load = async ({ url, fetch }) => {
